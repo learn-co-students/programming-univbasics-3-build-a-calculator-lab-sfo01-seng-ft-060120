43c1d18c6401bed0c37be241be5bec5db0913931
Iona <ionabrabender@gmail.com>
2020-04-24 01:07:54 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,29 @@
 def addition(num1, num2)
-
+  num3 = num1 + num2
+  p num3
 end
 
 def subtraction(num1, num2)
-
+  num3 = num1 - num2
+  p num3
 end
 
 def division(num1, num2)
-
+  num3 = num1 / num2
+  p num3
 end
 
 def multiplication(num1, num2)
-
+  num3 = num1 * num2
+  p num3
 end
 
 def modulo(num1, num2)
-
+  num3 = num1 % num2
+  p num3
 end
 
 def square_root(num)
-
+  root = Math.sqrt(num)
+  p root
 end
